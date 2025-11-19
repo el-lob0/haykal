261370427ef817fb15d93753611278d76b63f3b8
Anas <anas.siraj.souabni@gmail.com>
2025-11-19 22:27:23 +0300

    haykal init

---
+++ include/haykal.h
@@ -0,0 +1,6 @@
+#ifndef HAYKAL_H
+#define HAYKAL_H
+
+void greet_library(const char* name);
+
+#endif // HAYKAL_H
---
+++ src/haykal.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "haykal.h"
+
+void greet_library(const char* name) {
+    printf("Library message: Hello, %s! This is haykal.\n", name);
+}
